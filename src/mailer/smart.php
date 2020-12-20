14475e18c27d1ca950f8b83b0a12e934b8d0d3cc
niminskiy <s.niminskiy@gmail.com>
2020-12-20 19:32:04 +0300

    вопросик

---
+++ src/mailer/smart.php
@@ -0,0 +1,46 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 's.niminskiy@gmail.com';                 // Наш логин
+$mail->Password = '1HJ39B6N';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('s.niminskiy@gmail.com', 'Pesto');   // От кого письмо 
+$mail->addAddress('s.niminskiy@gmail.com');     // Add a recipient
+$mail->addAddress('s.niminskiy@mail.ru');               // Name is optional
+// $mail->addAddress('admin@niminskiy.ru');               // Name is optional
+// $mail->addAddress('niminskiy@niminskiy.ru');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
